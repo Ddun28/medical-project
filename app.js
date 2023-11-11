41cdd5be09722983ec7276de10b3c47b12207ec2
Daniel Dun <danieldum28@gmail.com>
2023-11-10 20:46:10 -0400

    add

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
-<<<<<<< HEAD
 const cors = require('cors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-=======
->>>>>>> 51343543d614137ce8546b553479c0d1f654f90c
+
 const usersRouter = require('./controller/users');
 
 (async()=>{
@@ -20,12 +18,8 @@ const usersRouter = require('./controller/users');
     }
 })();
 
-<<<<<<< HEAD
 app.use(cors());
 app.use(cookieParser());
-=======
-
->>>>>>> 51343543d614137ce8546b553479c0d1f654f90c
 //rutas de frontend
 app.use('/', express.static(path.resolve('views', 'home')));
 app.use('/login', express.static(path.resolve('views', 'login')));
@@ -36,10 +30,7 @@ app.use('/admin', express.static(path.resolve('views', 'admin')));
 app.use('/historial', express.static(path.resolve('views', 'Principal', 'Historial')));
 app.use('/pagos', express.static(path.resolve('views', 'Principal', 'Pagos')));
 app.use('/img', express.static(path.resolve('img')));
-<<<<<<< HEAD
 app.use(morgan('tiny'));
-=======
->>>>>>> 51343543d614137ce8546b553479c0d1f654f90c
 
 app.use(express.json())
 
