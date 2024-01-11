c94c48db41d5e201ddeca75a5e56e06e9298195b
Daniel Dun <danieldum28@gmail.com>
2024-01-10 20:32:47 -0400

    add update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const PAGE_URL = process.env.NODE_ENV === 'production'
   ? 'http://localhost:3000/'
-  : 'http://localhost:3000/';
+  : 'https://medical-project-kx19.onrender.com';
 
   module.exports = {PAGE_URL}
