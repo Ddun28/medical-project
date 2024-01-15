94f715a387615ec16fd35e6a980ebb469d145347
Daniel Dun <danieldum28@gmail.com>
2024-01-15 13:05:15 -0400

    add

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const PAGE_URL = process.env.NODE_ENV === 'production'
   ? 'https://medical-project-kx19.onrender.com'
-  : 'http://localhost:3000/';
+  : 'http://localhost:3000';
 
   module.exports = {PAGE_URL}
--- views/verify/index.js
+++ views/verify/index.js
@@ -4,7 +4,7 @@ const textInfo = document.querySelector('#text-info');
   try {
     const token = window.location.pathname.split('/')[3];
     const id = window.location.pathname.split('/')[2];
-    await axios.patch(`/api/users/${id}/${token}`);
+    const {data} = await axios.patch(`/api/users/${id}/${token}`);
     window.location.pathname = '/login/';
   } catch (error) {
     textInfo.innerHTML = error.response.data.error;
